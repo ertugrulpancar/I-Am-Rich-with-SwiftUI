93c241915a85cb4b4415cc110661d2bcb64a0136
Ertuğrul Pancar <21220030023@mersin.edu.tr>
2024-09-02 22:01:00 +0300

    README file update.

--- I Am Rich/ContentView.swift
+++ I Am Rich/ContentView.swift
@@ -9,13 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        ZStack {
+            Color(.systemTeal).ignoresSafeArea()
+                VStack {
+                    
+                    Image(systemName: "globe")
+                        .imageScale(.large)
+                        .foregroundStyle(.tint)
+                    VStack {
+                        Text("I Am Rich").font(.system(size: 40)).fontWeight(.bold).foregroundColor(Color.white)
+                        Image("diamond").resizable().aspectRatio(contentMode: .fit)
+                            .frame(width: 200, height: 200, alignment: .center)
+                    }
+                }
+            }
+        
     }
 }
 
--- README.md
+++ README.md
@@ -2,4 +2,5 @@ I AM RICH PROJECT
 ANGEL YOU BOOTCAMP
 
 
-/Users/ertugrul/Desktop/Ekran Resmi 2024-09-02 21.55.21.png
+
+![Pages ScreenShot](/Users/ertugrul/Desktop/Ekran Resmi 2024-09-02 21.55.21.png)
